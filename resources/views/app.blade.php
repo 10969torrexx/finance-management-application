2a8ac11aeff31bb56e45b26d50c8495c0630f1f4
10969torrexx <pablitojrtorre@gmail.com>
2025-05-03 21:27:01 +0800

    home/sub: set up rgba color

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,11 +25,11 @@
         {{-- Inline style to set the HTML background color based on our theme in app.css --}}
         <style>
             html {
-                background-color: oklch(1 0 0);
+                background-color:rgb(255,255, 255);
             }
 
             html.dark {
-                background-color: oklch(0.145 0 0);
+                background-color: rgba(37, 37, 37, 1);
             }
         </style>
 
