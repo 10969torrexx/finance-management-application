9600e778455ffbed91e47650810ecfad2273abe8
Torrexx <fdc.pablitot@gmail.com>
2025-05-07 09:44:19 +0800

    home/sub: change auth layout

--- resources/js/layouts/auth-layout.tsx
+++ resources/js/layouts/auth-layout.tsx
@@ -3,8 +3,8 @@ import AuthCardLayout from './auth/auth-card-layout';
 
 export default function AuthLayout({ children, title, description, ...props }: { children: React.ReactNode; title: string; description: string }) {
     return (
-        <AuthCardLayout title={title} description={description} {...props}>
+        <AuthLayoutTemplate title={title} description={description} {...props}>
             {children}
-        </AuthCardLayout>
+        </AuthLayoutTemplate>
     );
 }
